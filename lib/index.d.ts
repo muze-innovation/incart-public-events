b3e632dc47030d791b939c4a2b041a51c597b58b
ziitel2 <37287332+ziitel2@users.noreply.github.com>
2021-02-12 19:45:57 +0700

    added paidOrder type also changed baseEvent (#1)
    * added paidOrder type also changed baseEvent
    * removed bulk event data in publicHook type
    * build lib
    * v0.0.3
    
    Co-authored-by: Nattanon Piamwilaikul <nattanon@muze.co.th>
    Co-authored-by: TimeK <time@muze.co.th>

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,3 @@
-export interface InCartPublicBaseEvent {
-    storeId: string;
-}
 export interface Stock {
     sku: string;
     qty: number | null;
@@ -8,17 +5,17 @@ export interface Stock {
     stockType: string;
 }
 export interface InCartPublicUpdateStockEventPayload {
+    event: 'updateStock';
     stocks: Stock[];
     updateIn: string;
 }
-export interface InCartPublicUpdateStocksEvent extends InCartPublicBaseEvent {
-    payloads: InCartPublicUpdateStockEventPayload[];
-    event: 'updateStock';
+export interface InCartPublicPaidOrderEventPayload {
+    event: 'paidOrder';
+    orderToken: string;
+    orderId: number;
 }
-export interface InCartPublicUpdateOrderEventPayload {
+export declare type IncartPublicHookPayload = InCartPublicUpdateStockEventPayload | InCartPublicPaidOrderEventPayload;
+export interface InCartPublicEvent {
+    storeId: string;
+    payloads: IncartPublicHookPayload[];
 }
-export interface InCartPublicUpdateOrdersEvent extends InCartPublicBaseEvent {
-    payloads: InCartPublicUpdateOrderEventPayload[];
-    event: 'updateOrder';
-}
-export declare type InCartPublicEvent = InCartPublicUpdateStocksEvent | InCartPublicUpdateOrdersEvent;
